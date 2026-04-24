f88d33993b34002a7688e4e087f147f7f3593afc
sog <4611hd@gmail.com>
2026-04-24 12:14:41 -0400

    GAAA

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
 use bevy::{
-    color::palettes::css, ecs::entity_disabling::Disabled, prelude::*, window::WindowResolution,
+    prelude::*, window::WindowResolution,
 };
 use threshold_post_process::{ThresholdPostProcessPlugin, ThresholdSettings};
 
@@ -29,7 +29,9 @@ fn main() {
     );
 
     app.add_systems(Startup, setup);
-    app.add_systems(FixedUpdate, (circle_move, toggle_disable_system));
+    app.add_systems(FixedUpdate, ui_circle_move);
+    app.add_observer(fullscreen_sprite_on_add);
+    app.add_systems(Update, (toggle_fullscreen_sprite_system, fullsceen_sprite_system).chain());
 
     app.add_plugins((
         ThresholdPostProcessPlugin,
@@ -60,57 +62,61 @@ fn setup(mut commands: Commands, asset_server: Res<AssetServer>) {
             custom_size: Some(Vec2::new(512.0, 512.0)),
             ..default()
         },
+        FullscreenSprite::default()
     ));
-
-    // commands.spawn((
-    //     Name::new("WhiteCube"),
-    //     Transform {
-    //         translation: Vec3::new(64.0, 0.0, 1.0),
-    //         rotation: Quat::from_rotation_z(30.0),
-    //         ..default()
-    //     },
-    //     Sprite::from_color(css::WHITE, Vec2::new(32.0, 32.0)),
-    // ));
-    // commands.spawn((
-    //     Name::new("WhiteCube"),
-    //     Transform {
-    //         translation: Vec3::new(-64.0, 0.0, 1.0),
-    //         rotation: Quat::from_rotation_z(30.0),
-    //         ..default()
-    //     },
-    //     Sprite::from_color(css::WHITE, Vec2::new(32.0, 32.0)),
-    // ));
-
-    // commands.spawn((
-    //     Name::new("Logo"),
-    //     Transform {
-    //         translation: Vec3::new(256.0, -256.0, 1.0),
-    //         ..default()
-    //     },
-    //     Sprite::from_image(asset_server.load("images/bevy_icon_dark.png")),
-    // ));
 }
 
 #[derive(Component, Reflect, Debug)]
 #[reflect(Component)]
-pub struct ToggleDisabled(bool);
+struct FullscreenSprite {
+    is_fullscreen: bool,
+    original_size: Option<Vec2>,
+}
 
-fn toggle_disable_system(
-    mut commands: Commands,
-    keyboard_input: Res<ButtonInput<KeyCode>>,
-    query: Query<(Entity, &mut ToggleDisabled, Has<Disabled>)>,
+impl Default for FullscreenSprite {
+    fn default() -> Self {
+        return FullscreenSprite {
+            is_fullscreen: false,
+            original_size: None
+        }
+    }
+}
+
+fn fullscreen_sprite_on_add(
+    event: On<Add, FullscreenSprite>,
+    mut query_fullscreen_sprite: Query<&mut FullscreenSprite>,
+    query_sprite: Query<&Sprite>
 ) {
-    if keyboard_input.just_pressed(KeyCode::Space) {
-        for (entity, mut toggle_disabled, _) in query {
-            if toggle_disabled.0 == true {
-                println!("enable");
-                commands.entity(entity).remove::<Disabled>();
-                toggle_disabled.0 = false
-            } else {
-                println!("disable");
-                commands.entity(entity).insert(Disabled);
-                toggle_disabled.0 = true
-            }
+    let Ok(mut fullscreen_sprite) = query_fullscreen_sprite.get_mut(event.entity.entity()) else {
+        return
+    };
+    let Ok(sprite) = query_sprite.get(event.entity.entity()) else {
+        return
+    };
+
+    fullscreen_sprite.original_size = sprite.custom_size
+}
+
+fn fullsceen_sprite_system(
+    query: Query<(&mut Sprite, &FullscreenSprite)>,
+    window: Single<&Window>
+) {
+    for (mut sprite, fullscreen_sprite) in query {
+        if fullscreen_sprite.is_fullscreen {
+            sprite.custom_size = Some(window.size());
+        } else {
+            sprite.custom_size = fullscreen_sprite.original_size;
+        }
+    }
+}
+
+fn toggle_fullscreen_sprite_system(
+    keyboard_input: Res<ButtonInput<KeyCode>>,
+    query: Query<&mut FullscreenSprite>
+) {
+    if keyboard_input.just_pressed(KeyCode::KeyF) {
+        for mut fullscreen_sprite in query {
+            fullscreen_sprite.is_fullscreen = !fullscreen_sprite.is_fullscreen;
         }
     }
 }
@@ -133,7 +139,7 @@ impl Default for MoveInCircle {
     }
 }
 
-fn circle_move(query: Query<(&mut MoveInCircle, &mut UiTransform)>) {
+fn ui_circle_move(query: Query<(&mut MoveInCircle, &mut UiTransform)>) {
     for (mut move_in_circle, mut transform) in query {
         let vec = Vec2::from_angle(0.01745329 * move_in_circle.rotation) * move_in_circle.radius;
         transform.translation = Val2::new(px(vec.x), px(vec.y));
